aa8a5d37d590f459bfe947b188d68f18bb44e78f
kido <skido627@gmail.com>
2025-01-31 15:20:20 +0900

    ヘッダーのスクロール挙動実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,112 +76,22 @@ document.addEventListener('DOMContentLoaded', function () {
   $(window).scroll(function () {
     // スクロール位置を取得
     let scroll = $(window).scrollTop();
+    let fv = document.getElementById('fv');
+    let fvHeight = fv.clientHeight;
+    // console.log('高さ：' + fvHeight +'px');
 
     /*=================================================
-    メインビジュアルの拡大・縮小
+    ヘッダーの背景表示
     ===================================================*/
-    mv_scale(scroll);
-
-    /*=================================================
-    ロゴ、ハンバーガーメニューの表示
-    ===================================================*/
-    // スクロール位置が520pxを超えた場合
-    if (scroll > 520) {
+    // スクロール位置がFV(100vh)を超えた場合
+    if (scroll > fvHeight) {
       // ロゴとハンバーガ―メニュをfadeInで表示する
-      $('.header__logo').fadeIn(500);
-      $('.hamburger').fadeIn(500);
+      console.log('こえたよ');
+      document.querySelector('header').style.backgroundColor = '#282F35';
       // スクロール位置が520px未満の場合
     } else {
-      // ロゴとハンバーガ―メニュをfadeOutで非表示にする
-      $('.header__logo').fadeOut(500);
-      $('.hamburger').fadeOut(500);
+      document.querySelector('.header').style.backgroundColor = '';
+      console.log('こえてないよ');
     }
-
-    /*=================================================
-    サイドボタンを表示
-    ===================================================*/
-    // 画面下から#galleryまでの距離を取得
-    let gallery_position = $('#gallery').offset().top - $(window).height();
-    // 画面下から.access__headingまでの距離を取得
-    let access_position = $('.access__heading').offset().top - $(window).height();
-
-    // window.innerWidthで画面幅を取得
-    // PC表示の場合の処理（画面幅が900pxより大きい場合　※900pxはCSSのブレークポイントとあわせる）
-    if (window.innerWidth > 900) {
-      // #galleryが表示された場合（スクロール位置が、画面下から#galleryまでの距離を超えた場合）
-      if (scroll > gallery_position) {
-        // .access__headingが表示されるまでの間は、.side-btnを横からスライドさせて表示する
-        if (scroll < access_position) {
-          $('.side-btn').css({
-            transform: 'rotate(-90deg) translateY(0)',
-          });
-          // .access__headingが表示されたら、.side-btnをスライドさせて非表示にする
-        } else {
-          $('.side-btn').css({
-            transform: 'rotate(-90deg) translateY(60px)',
-          });
-        }
-        // #galleryが表示される前は、.side-btnをスライドさせて非表示にする
-      } else {
-        $('.side-btn').css({
-          transform: 'rotate(-90deg) translateY(60px)',
-        });
-      }
-    }
-
-    /*=================================================
-    Accessの背景画像を表示
-    ===================================================*/
-    // 画面下から#contactまでの距離を取得
-    let contact_position = $('#contact').offset().top - $(window).height();
-
-    // #accessが表示された場合
-    if (scroll > access_position) {
-      // #contactが表示されるまでの間は、背景画像をfadeInで表示する
-      if (scroll < contact_position) {
-        $('.access__bg').fadeIn(500);
-      } else {
-        $('.access__bg').fadeOut(500);
-      }
-      // #accessが表示される前の場合
-    } else {
-      // 背景画像を表示しない
-      $('.access__bg').fadeOut(500);
-    }
-  });
-
-  /*=================================================
-  画面読み込み時と画面幅変更時のイベント
-  ===================================================*/
-  $(window).on('load resize', function () {
-    // スクロール位置を取得
-    let scroll = $(window).scrollTop();
-
-    /*=================================================
-    メインビジュアルの拡大・縮小
-    ===================================================*/
-    mv_scale(scroll);
   });
 });
-
-/*=================================================
-メインビジュアルの拡大・縮小（共通処理）
-===================================================*/
-function mv_scale(scroll) {
-  // window.innerWidthで画面幅を取得
-  // PC表示の場合の処理（画面幅が900pxより大きい場合　※900pxはCSSのブレークポイントとあわせる）
-  if (window.innerWidth > 900) {
-    // メインビジュアルのCSS（width）を変更する
-    // widthの値をスクロール量にあわせて増やすことで画像を拡大させる
-    $('.main-visual__image').css({
-      width: 100 / 3 + scroll / 10 + '%',
-    });
-    // スマホ表示の場合の処理（画面幅が900px以下の場合）
-  } else {
-    // メインビジュアルのCSS（width）を変更する
-    // widthの値をスクロール量にあわせて減らすことで画像を縮小させる
-    $('.main-visual__image').css({
-      width: 100 - scroll / 10 + '%',
-    });
-  }
-}
