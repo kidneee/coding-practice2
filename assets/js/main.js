9ea92db7ecefdacf647a28e40b3e166015868c7e
kido <skido627@gmail.com>
2025-02-03 10:00:52 +0900

    1/31分

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,4 +94,16 @@ document.addEventListener('DOMContentLoaded', function () {
       console.log('こえてないよ');
     }
   });
+
+  //Slick
+  $('.slick').slick({
+    fade: true,
+    autoplay: true,
+    speed: 1500,
+    autoplaySpeed: 2000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    arrows: false,
+  });
+
 });
